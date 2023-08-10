4b914c74159ea84da808de84fc5a5a4cf1701bf2
DavV <dvartanian@team.amocrm.com>
2023-08-10 11:57:09 +0300

    Add logger in SumHandler with log current get params sum

--- src/Sync/Handlers/SumHandler.php
+++ src/Sync/Handlers/SumHandler.php
@@ -9,14 +9,31 @@ use Laminas\Diactoros\Response\JsonResponse;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\RequestHandlerInterface;
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
+use Monolog\Handler\FirePHPHandler;
 
 
 class SumHandler implements RequestHandlerInterface
 {
 
+    protected $log;
+
+    public function logger() {
+        $currentDate = date('Y-m-d');
+        $this->log = new Logger('sum_logger');
+        $this->log->pushHandler(new StreamHandler("./logs/{$currentDate}/requests.log", Logger::DEBUG));
+        $this->log->pushHandler(new FirePHPHandler);
+
+    }
     public function handle(ServerRequestInterface $request): ResponseInterface {
+        $sum = array_sum(array_values($request->getQueryParams()));
+        $this->logger();
+        $this->log->info($sum);
+
         return new JsonResponse([
-            'sum' => array_sum(array_values($request->getQueryParams()))
+            'sum' => $sum,
         ]);
+
     }
 }
