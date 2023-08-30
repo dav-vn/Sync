44abd40c904be626f0c23e934e8b095aa55e0535
DavV <dvartanian@team.amocrm.com>
2023-08-30 12:26:39 +0300

    FEAT: Add new route to ger formated accounts list

---
+++ src/Sync/Api/GetAccountService.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace Sync\Api;
+
+use Sync\Models\Account;
+
+/**
+ * Class GetAccountService.
+ *
+ * @package Sync\Api
+ */
+class GetAccountService
+{
+    /** @var DatabaseConnectService Подключение к базе данных */
+    protected DatabaseConnectService $databaseConnect;
+
+    public function getAccounts(): array
+    {
+        $this->databaseConnect = new DatabaseConnectService;
+
+        $accountsData = Account::with(['integrations', 'contacts', 'accesses'])->get()->toArray();
+
+        $formattedAccounts = [
+            'status' => 'success',
+            'data' => [
+                'accounts' => [
+                    'all' => [],
+                    'with_accesses' => []
+                ]
+            ]
+        ];
+
+        foreach ($accountsData as $accountData) {
+            $accountName = $accountData['name'];
+
+            $integrations = [];
+            foreach ($accountData['integrations'] as $integration) {
+                $integrations[] = $integration['integration_id'];
+            }
+
+            $contactsCount = count($accountData['contacts']);
+
+            $apiKey = $accountData['accesses']['api_key'];
+
+            $accountInfo = [
+                'amo_id' => $accountData['amo_id'],
+                'integration_id' => $integrations,
+                'contacts_count' => $contactsCount,
+                'api_key' => $apiKey
+            ];
+
+            $formattedAccounts['data']['accounts']['all'][] = [
+                $accountName => $accountInfo
+            ];
+
+            if (!empty($accountData['accesses'])) {
+                $formattedAccounts['data']['accounts']['with_accesses'][] = $accountName;
+            }
+        }
+
+        return $formattedAccounts;
+    }
+}
+
+
+
